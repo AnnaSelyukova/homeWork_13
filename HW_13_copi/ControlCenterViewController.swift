ce39bc17efea338ac507b7f119de17cea3875c4d
AnnaSelyukova <asselyukova@gmail.com>
2025-05-22 16:04:31 +0300

    HW_13
    UITableView

---
+++ HW_13_copi/ControlCenterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ControlCenterViewController.swift
+//  HW_13_copi
+//
+//  Created by macbook on 22.05.2025.
+//
+
+import Foundation
---
+++ HW_13_copi/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  HW_13_copi
+//
+//  Created by macbook on 22.05.2025.
+//
+
+import Foundation
